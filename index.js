c324f8c60909ced262abb0b80ff73bc9b81003b7
Clarence Jordan <clarencejordan087@gmail.com>
2024-09-09 00:06:41 -0500

    add db

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+import express from "express";
 import { chromium } from "playwright";
-import { parsifyJson, tofile } from "./tofile.js";
-async function main() {
+import connectDB from "./db/connectTodb.js";
+import User from "./db/models/User.js";
+const app = express();
+
+async function main(jobtitle) {
   // Launch a new instance of a Chromium browser with headless mode
   // disabled for visibility
   const browser = await chromium.launch({
@@ -8,7 +12,6 @@ async function main() {
     slowMo: 350,
   });
 
-  const jobtitle = "software developer";
   // Create a new Playwright context to isolate browsing session
   const context = await browser.newContext();
   // Open a new page/tab within the context
@@ -27,16 +30,38 @@ async function main() {
 
   await page.press(searchBar, "Enter");
 
-  //   const jobcontainer = page.getByTestId("indeedApply");
-  // prettier-ignore
+  const nextPageBtn = "pagination-page-next";
   const div = page.locator("[id=mosaic-provider-jobcards]");
+  // todo put this in another file
 
-  const itterater = await div.evaluate(() => {
+  // next page
+  let hasNexPage = true;
+  let count = 0;
+  while (hasNexPage && count < 5) {
+    await getLinks(div);
+    const nextPage = page.getByTestId(nextPageBtn);
+    const isDisabled = await nextPage.evaluate((btn) => btn.disabled);
+    if (!nextPage || isDisabled) {
+      hasNexPage = false;
+    } else {
+      await nextPage.click();
+      await page.waitForLoadState("domcontentloaded");
+    }
+    console.log(count);
+    count++;
+  }
+
+  //   await browser.close();
+}
+
+const getLinks = async (div) => {
+  const jobs = await div.evaluate(() => {
     // jobs data arr
     const arr = [];
     // sort divs of jobs and get only easy apply and the name
     Array.from(document.getElementsByClassName("slider_container")).map((a) => {
       // todo recursion
+
       if (a.innerHTML.includes("Easily apply")) {
         // if the div have easy apply then get the link and job title
         const joblink = a.querySelector("a[href]");
@@ -44,27 +69,42 @@ async function main() {
         const jobTitle = a.querySelector("h2,h3");
         // if theses exest add them to the array
         if (joblink && jobTitle) {
-          arr.push({ jobname: jobTitle.innerText.trim(), link: joblink.href });
+          try {
+            arr.push({
+              jobname: jobTitle.innerText.trim(),
+              link: joblink.href,
+            });
+
+            console.log("Saved job");
+          } catch (error) {
+            console.log(error, "Job");
+          }
         }
       }
     });
-
     return arr;
   });
+  console.log(jobs);
+  for (let job of jobs) {
+    try {
+      const newUser = new User({
+        jobname: job.jobname,
+        link: job.link,
+      });
 
+      await newUser.save();
+      console.log(`Saved job: ${job.jobname}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   // Convert the results to JSON and save to a file
-  const jsonJobs = parsifyJson(itterater);
-  tofile(jsonJobs);
-
-  //   await browser.close();
-}
-
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+};
 // Execute the main function
 try {
-  main();
+  connectDB();
+  app.listen(3000, () => console.log("server running on port 3000"));
+  main("software developer");
 } catch (error) {
   console.log(error);
 }
